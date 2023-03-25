3d912c1c79baaa3af74b816cac5576670f6d6a97
Thomas Orthbandt <torthbandt@gmail.com>
2023-03-25 13:36:27 -0700

    Updated Styling

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME 				  	from '../../assets/thomas-orthbandt-headshot.jpg'
+import ME 				  	from '../../assets/thomas-orthbandt-headshot.webp'
 import {FaAward} 			from 'react-icons/fa'
 import {FiUsers} 			from 'react-icons/fi'
 import {VscFolderLibrary} 	from 'react-icons/vsc'
@@ -8,13 +8,12 @@ import {VscFolderLibrary} 	from 'react-icons/vsc'
 const About = () => {
 	return (
 		<section id="about">
-			<h5>Get To Know</h5>
 			<h2>About Me</h2>
 
 			<div className="container about__container">
 				<div className="about__me">
 					<div className="about__me-image">
-						<img src={ME} alt="Thomas Orthbandt" />
+						<img src={ME} alt="Thomas Orthbandt" width="374" height="374" />
 					</div>
 				</div>
 				<div className="about__content">
@@ -37,9 +36,20 @@ const About = () => {
 					</div>
 
 					<p>
+					I'm a highly skilled and experienced <strong>Website Manager</strong> and <strong>Front-end Developer</strong> with over 20+ years of experience. I'm proficient in HTML5, CSS3, JS (vanilla & React), and other relevant programming languages such as PHP.</p>
+					
+					<p> I'm adept at utilizing content management systems (CMS) to manage and maintain websites, building Wordpress Themes, implementing SEO and website optimization strategies, as well as executing digital marketing plans to drive website page views and conversions.</p>
+
+					<p>I have a lot of experience with Google Analytics (GA UA & GA4), Google Tag Manager implementing dataLayers and Custom Dimensions, Google Optimize for Conversion Rate Optimization AB testing and Personalization.  I enjoy using real data to better serve the end-users.</p>
+
+					<p>I can help any company get the best out of their website and grow their user base and conversions.  If interested ...</p>
+
+					{/*
+					<p>
 					Self-motivated technology professional with deep knowledge and proficiency in HTML, CSS, JavaScript, and mobile responsive web development. Enjoy working collaboratively with Design, Marketing, Engineering, and Product teams to produce effective web campaigns.</p>
 					<p><strong>Specialties:</strong> Latest web technologies, strategic and creative demand generation experience, lead maturation, analytical and front-end web development coding.
 					</p>
+					*/}
 
 					<a href="#contact" className='btn btn-primary'>Let's Chat</a>
 
