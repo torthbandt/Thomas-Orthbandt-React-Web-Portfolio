32c11edb9a2f0f3ec471d6d3b80ba7a9a53b39ff
Thomas Orthbandt <torthbandt@gmail.com>
2023-03-25 13:40:11 -0700

    Updated Styling

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
-import AVTR2 from '../../assets/avatar2.jpg'
-import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR1 from '../../assets/mike.webp'
+import AVTR2 from '../../assets/daniel.webp'
+import AVTR3 from '../../assets/sandy.webp'
 
 // import Swiper core and required modules
 import { Pagination } from 'swiper';
@@ -16,25 +16,24 @@ import 'swiper/css/pagination';
 const data = [
   {
     avatar: AVTR1,
+    name: 'Mike Serpe',
+    review: "Thomas feverishly supported our team in the capacity of Global Marketing Manager. Our team was highly successful in marketing and business development of all services offerings under the Oracle University umbrella. This included but is not limited to generating $200-600M per annum in revenues from educational services, courseware licensure, certification programs/exams."
+  },
+  {
+    avatar: AVTR2,
     name: 'Daniel Chin',
     review: "If you think of passion, honesty, and relability, you get Thomas. Thomas has done a great job for me in his web development and design. Now if only there were more Thomas's out there."
   },
   {
-    avatar: AVTR2,
+    avatar: AVTR3,
     name: 'Sandy Chu',
     review: "Thomas has helped me set up many marketing campaigns throughout his tenure at Intuit. I've always found him very pleasant and helpful even with tight turnaround times. I appreciate his service-oriented mentality and strong business partnership."
-  },
-  {
-    avatar: AVTR3,
-    name: 'Mike Serpe',
-    review: "Thomas feverishly supported our team in the capacity of Global Marketing Manager. Our team was highly successful in marketing and business development of all services offerings under the Oracle University umbrella. This included but is not limited to generating $200-600M per annum in revenues from educational services, courseware licensure, certification programs/exams."
   }
 ]
 
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      <h5>Reviews from Clients</h5>
       <h2>Testimonials</h2>
 
       <Swiper className="container testimonials__container"
@@ -48,7 +47,7 @@ const Testimonials = () => {
           return (
             <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
-                <img src={avatar} alt="{name}" />
+                <img src={avatar} alt={name} width="132" height="132" />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className='client__review'>{review}</small>
