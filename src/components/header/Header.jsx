4820caa1f4c65687c6201d56c3648fa712e7f4af
Thomas Orthbandt <torthbandt@gmail.com>
2023-08-27 16:42:05 -0700

    added new headshot to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-//import ME from '../../assets/me-transparent.webp'
+import ME from '../../assets/thomas-orthbandt-headshot.webp'
 import HeaderSocials from './HeaderSocials'
-//import {BsFillArrowDownSquareFill} from 'react-icons/bs'
+import {BsFillArrowDownSquareFill} from 'react-icons/bs'
 
 
 const Header = () => {
@@ -22,11 +22,11 @@ const Header = () => {
           
         </div>
 
-        {/*<div className="me">
+        <div className="me">
           <img src={ME} alt="Thomas Orthbandt" width="353" height="353" />
         </div>
 
-        <a href="#contact" className="scroll__down" title="Scroll Down Page"><BsFillArrowDownSquareFill/></a>*/}
+        <a href="#contact" className="scroll__down" title="Scroll Down Page"><BsFillArrowDownSquareFill/></a>
       </div>
     </header>
   )
